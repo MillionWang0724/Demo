d7e414ae1520b6e21017b064176300d4ca6e150b
MillionWang0724 <77611577+MillionWang0724@users.noreply.github.com>
2021-06-24 10:23:32 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,17 @@
+package com.huami.ast;
+/**
+ * @ClassName Test.java
+ * @author admin
+ * @version 1.0.0
+ * @Description TODO
+ * @createTime 2021年06月23日 17:25:00
+ */
+public class Test {
+    public static void BubbleSortFloat2(float[] num) {
+        int a = 1;
+        int b = a + 2;
+        if(true){
+            System.out.println("Hello huami!");
+        }
+    }
+}
